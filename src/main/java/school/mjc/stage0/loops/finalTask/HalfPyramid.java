9fe7568c39d7b7cc09d1b218f95fc245b2b79133
Vladislav Samorukov <smrk.vld@gmail.com>
2023-02-17 01:30:58 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            int offset = cathetusLength - i;
+            System.out.print(" ".repeat(offset));
+            System.out.println("*".repeat(i));
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            int offset = cathetusLength - i;
+            System.out.print(" ".repeat(offset));
+            for (int k = i; k >= -i; k--) {
+                if (k == 0 || k == -1) {
+                    continue;
+                }
+                System.out.print(Math.abs(k));
+            }
+            System.out.println(" ".repeat(offset));
+        }
     }
 
     public static void main(String[] args) {
